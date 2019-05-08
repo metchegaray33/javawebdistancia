468f9e88945d7fe8ab22f243c0a11d5726196f01
metchegaray <maricel.etchegaray@gmail.com>
2019-05-07 21:29:31 -0300

    prueba

--- src/main/java/clase1/EmbeddingJettyWithServlet.java
+++ src/main/java/clase1/EmbeddingJettyWithServlet.java
@@ -4,22 +4,29 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
-
+/**
+ * Maricel
+ * @author Usuario
+ *
+ */
 public class EmbeddingJettyWithServlet {
 
     public static void main(String[] args) throws Exception {
 
+    	//las siguientes 4 lineas leventan un server.
         Server server = new Server(8080);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
-        context.setContextPath("/hello");
+        context.setContextPath("/hello");//digo donde voy a estar escuchando siempre en /hello aca
 
         server.setHandler(context);
 
-
-        context.addServlet(new ServletHolder(new HelloServlet()), "/simple");
-        context.addServlet(new ServletHolder(new HelloHtmlServlet()), "/html");
+         //le decimos al jetty cuales son los servlet que hay que instalar y le decimos la expresion regular que define 
+        //que request van a ser atendidos por este servlet.Puedo tener distintos path y ser atendidos.
+       // context.addServlet(new ServletHolder(new HelloServlet()), "/simple");
+        context.addServlet(new ServletHolder(new HelloServlet()), "/*"); //si le pongo el asterisco invoco con http://localhost:8080/hello/
+       // context.addServlet(new ServletHolder(new HelloHtmlServlet()), "/html");
 
         server.start();
 
