86d9fd28f873f59f2e2075b27b80f56568047f05
metchegaray <maricel.etchegaray@gmail.com>
2019-06-08 20:32:36 -0300

    nuevo  mensaje

--- src/main/java/clase1/EmbeddingJettyWithServlet.java
+++ src/main/java/clase1/EmbeddingJettyWithServlet.java
@@ -7,6 +7,7 @@ import org.eclipse.jetty.servlet.ServletHolder;
 /**
  * desdemasternewamaster1233tt
  * @author Usuario
+ * http://localhost:8080/hello/simple
  *
  */
 public class EmbeddingJettyWithServlet {
--- src/main/java/clase1/HelloServlet.java
+++ src/main/java/clase1/HelloServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 /**
- * Servlet hello world
  * @author Usuario
  *
  */
