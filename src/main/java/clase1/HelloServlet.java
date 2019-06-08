288274ee655edd7be270b573db1b06532994e9d0
metchegaray <maricel.etchegaray@gmail.com>
2019-06-08 20:29:00 -0300

    hello world

--- src/main/java/clase1/HelloServlet.java
+++ src/main/java/clase1/HelloServlet.java
@@ -5,7 +5,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+/**
+ * Servlet hello world
+ * @author Usuario
+ *
+ */
 public class HelloServlet extends HttpServlet {
 
     private static final long serialVersionUID = -6154475799000019575L;
